1680fc23fcd6f52d0ae7714a86419201ebe11637
NSheketa <natalya.sheketa@gmail.com>
2020-03-29 00:50:26 +0200

    Added Product Detail page

--- js/main.js
+++ js/main.js
@@ -314,6 +314,35 @@ $(document).ready(function () {
     nextArrow: $('.other-news__controls').find('.news-list__arrow--next'),
   });
 
+  $('.pdetail-related__carousel').slick({
+    arrows: true,
+    dots: false,
+    fade: false,
+    mobileFirst: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive:[
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      },
+    ],
+    prevArrow: $('.pdetail-related__controls').find('.pdetail-related__arrow--prev'),
+    nextArrow: $('.pdetail-related__controls').find('.pdetail-related__arrow--next'),
+  });
+
+
   function heroContentAnimation() {
     const heroContentTl = new TimelineMax({
       repeat: 0,
